f6da0a911d38b3ccfdb5b7a4714bc64563d54ec6
wouterpot2@gmail.com <wouterpot2@gmail.com@3d3196db-9e89-b88c-b129-4032459edf27>
2014-03-28 08:02:07 +0000

    - Removed Quantity as it is redundant type

--- test/com/wouterpot/makeyourshoppinglist/test/UnitTest.java
+++ test/com/wouterpot/makeyourshoppinglist/test/UnitTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.wouterpot.makeyourshoppinglist.server.datastore.QuantityType;
 import com.wouterpot.makeyourshoppinglist.server.datastore.Unit;
 import com.wouterpot.makeyourshoppinglist.server.datastore.UnitType;
 
@@ -11,8 +12,8 @@ public class UnitTest {
 
 	@Test
 	public void convertTo() {
-		Unit unit1 = new Unit(UnitType.l, 2.0);
-		Unit unit2 = new Unit(UnitType.dl, 3.0);
+		Unit unit1 = new Unit(QuantityType.Volume, UnitType.l, 2.0);
+		Unit unit2 = new Unit(QuantityType.Volume, UnitType.dl, 3.0);
 		Unit converted = unit2.convertTo(unit1);
 		assertEquals(0.3, converted.getAmount(), 0.0000001);
 		assertEquals(UnitType.l, converted.getUnitType());
@@ -20,17 +21,17 @@ public class UnitTest {
 	
 	@Test
 	public void addDifferentUnits() {
-		Unit unit1 = new Unit(UnitType.l, 2.0);
-		Unit unit2 = new Unit(UnitType.ml, 1.0);
+		Unit unit1 = new Unit(QuantityType.Volume, UnitType.l, 2.0);
+		Unit unit2 = new Unit(QuantityType.Volume, UnitType.ml, 1.0);
 		assertEquals("2001ml", unit1.add(unit2).toString());
 	}
 
 	@Test
 	public void addFourDifferentUnits() {
-		Unit unit1 = new Unit(UnitType.l, 1.0);
-		Unit unit2 = new Unit(UnitType.dl, 1.0);
-		Unit unit3 = new Unit(UnitType.cl, 1.0);
-		Unit unit4 = new Unit(UnitType.ml, 1.0);
+		Unit unit1 = new Unit(QuantityType.Volume, UnitType.l, 1.0);
+		Unit unit2 = new Unit(QuantityType.Volume, UnitType.dl, 1.0);
+		Unit unit3 = new Unit(QuantityType.Volume, UnitType.cl, 1.0);
+		Unit unit4 = new Unit(QuantityType.Volume, UnitType.ml, 1.0);
 		Unit added = unit1.add(unit2);
 		assertEquals("11dl", added.toString());
 		added = added.add(unit3);
