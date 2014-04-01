0beee684c6f601f1b94623f0b08771e918c3756c
wouterpot2@gmail.com <wouterpot2@gmail.com@3d3196db-9e89-b88c-b129-4032459edf27>
2014-04-01 15:30:17 +0000

    - strike through shopping item when checked

--- trunk/src/com/wouterpot/makeyourshoppinglist/client/CheckboxClickHandler.java
+++ trunk/src/com/wouterpot/makeyourshoppinglist/client/CheckboxClickHandler.java
@@ -1,5 +1,6 @@
 package com.wouterpot.makeyourshoppinglist.client;
 
+import com.google.gwt.dom.client.Style.TextDecoration;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.CheckBox;
@@ -15,7 +16,9 @@ public class CheckboxClickHandler implements ClickHandler {
 
 	@Override
 	public void onClick(ClickEvent event) {
-		product.setVisible(!((CheckBox)event.getSource()).getValue());
+		CheckBox source = (CheckBox)event.getSource();
+		product.setVisible(!source.getValue());
+		source.getElement().getStyle().setTextDecoration(source.getValue() ? TextDecoration.LINE_THROUGH : TextDecoration.NONE);
 	}
 
 }
